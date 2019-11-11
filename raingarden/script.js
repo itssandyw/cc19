be538c4570c1dd7c04da72d9553a2e3ea5ca1d77
itssandyw <54820636+itssandyw@users.noreply.github.com>
2019-11-11 13:53:21 -0500

    Update script.js

--- raingarden/script.js
+++ raingarden/script.js
@@ -1,27 +1,10 @@
 $(document).ready(function(){
 
- // number of drops created.
-var nbDrop = 90; 
+$(window).scroll(function () {
+		var scrollpos = $(window).scrollTop();
+		$('.rain').css({
+			'height': +(scrollpos) + 'windowheight'
 
-// function to generate a random number range.
-function randRange( minNum, maxNum) {
-  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
-}
-
-// function to generate drops
-function createRain() {
-
- for( i=1;i<nbDrop;i++) {
- var dropLeft = randRange(0,1600);
- var dropTop = randRange(-1000,1400);
-
- $('.rain').append('<div class="drop" id="drop'+i+'"></div>');
- $('#drop'+i).css('left',dropLeft);
- $('#drop'+i).css('top',dropTop);
- }
-
-}
-// Make it rain
-createRain();
-
-});
+		});
+	});
+ });
