7ce3e8a1f0334782b4c6ac0e8c96de9399aadd34
itssandyw <54820636+itssandyw@users.noreply.github.com>
2019-12-09 23:03:31 -0500

    Update wtd.js

--- final/wtd.js
+++ final/wtd.js
@@ -3,12 +3,15 @@ $(document).ready(function(){
 	$(".pin").hide();
 	
 	$(".cell:nth-child(2n+2)").mouseover(function(){
-		$(".cell:nth-child(2n+2)").css("background-color", "lightgray");
+		$(".cell:nth-child(2n+2)").css("background-color", "#f5a397");
 		$(".pin").show();
 	});
 	$(".cell:nth-child(2n+2)").mouseout(function(){
-		$(".cell:nth-child(2n+2)").css("background-color", "black");
+		$(".cell:nth-child(2n+2)").css("background-color", "#cdb8f2");
 		$(".pin").hide();
+	});
+	$("section").mouseover(function(){
+		$("section").hide();
 	});
 	
  });
