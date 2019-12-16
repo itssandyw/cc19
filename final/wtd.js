65f64b77f6abbd81a035c654170e89d1d0f6930e
itssandyw <54820636+itssandyw@users.noreply.github.com>
2019-12-16 15:18:29 -0500

    Update wtd.js

--- final/wtd.js
+++ final/wtd.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
 	$(".pin").hide();
 	
-	$(".cell:nth-child(2n+2)").mouseover(function(){
-		$(".cell:nth-child(2n+2)").css("background-color", "#f5a397");
-		$(".pin").show();
+	$("a.cell").mouseover(function(){
+		$(this).css("background-color", "#f5a397");
+		$(this).find(".pin").show();
 	});
-	$(".cell:nth-child(2n+2)").mouseout(function(){
-		$(".cell:nth-child(2n+2)").css("background-color", "#cdb8f2");
-		$(".pin").hide();
+	$("a.cell").mouseout(function(){
+		$(this).css("background-color", "#cdb8f2");
+		$(this).find(".pin").hide();
 	});
 	$("section").mouseover(function(){
 		$("section").hide();
